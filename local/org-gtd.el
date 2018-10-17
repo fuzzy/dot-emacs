ba5f1f9daeb9669a242e72bd0d63ad14a21ade7f
Mike 'Fuzzy' Partin <mpartin@starbucks.net>
2018-10-17 13:58:16 -0700

    syntax fix

--- local/org-gtd.el
+++ local/org-gtd.el
@@ -96,4 +96,4 @@ PRIORITY may be one of the characters ?A, ?B, or ?C."
 (define-key thwap-map (kbd "r u") 'elfeed-update)
 (define-key thwap-map (kbd "r a") 'elfeed-add-feed)
 (define-key thwap-map (kbd "r d") 'elfeed-kill-buffer)
-(define-key thwap-map (kbd "r f") 'elfeed-search-live-filter
+(define-key thwap-map (kbd "r f") 'elfeed-search-live-filter)
